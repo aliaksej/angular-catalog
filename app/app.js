e3e174de2f052713aa037bab4f496dc3bd946065
unknown <akatarkevich@akatarkevich-mi-1037.ELTEGRA.BY>
2015-01-14 10:44:38 +0300

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* App Module */
+
+var catalogApp = angular.module('catalogApp', []);
+
+catalogApp.controller('CatalogCtrl', ['$scope', '$http',
+  function($scope, $http) {
+
+    var root = {name: 'Catalog'};
+    $scope.breadcrumbs = [ root ];
+
+    $http.get('data/catalog.json').success(function(data) {
+      $scope.items = data.items;
+      root.items = data.items;
+    });
+
+    $scope.browseCategory = function () {
+      $scope.items = this.item.items;
+      $scope.breadcrumbs.push(this.item);
+    };
+
+    $scope.openLink = function () {
+      alert('Navigating to link: ' + this.item.name);
+    };
+
+    $scope.onBreadcrumbClick = function () {
+      $scope.items = this.breadcrumb.items;
+      var position = $scope.breadcrumbs.indexOf(this.breadcrumb);
+      $scope.breadcrumbs = $scope.breadcrumbs.slice(0, position + 1);
+    };
+  }]);
